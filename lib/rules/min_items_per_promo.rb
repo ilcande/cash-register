b5dd2060b8c1dac3c39c500c3dc3f9c5b7a0aa0d
Luca Candela <ilcande_prime@live.it>
2022-11-23 09:34:16 +0100

    setup min items per promo class and write specs for it

---
+++ lib/rules/min_items_per_promo.rb
@@ -0,0 +1,10 @@
+class MinItemsPerPromo
+  def initialize(min_items_per_promo, value)
+    @min_items_per_promo = min_items_per_promo
+    @value = value
+  end
+
+  def applies_to(items)
+    items.size >= @min_items_per_promo
+  end
+end
